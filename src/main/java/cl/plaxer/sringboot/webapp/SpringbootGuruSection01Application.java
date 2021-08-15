6de7bbe05574a4c15608866d4c391d0f51a0d5b7
Emmanuel Nieto <enieto@alumnos.santotomas.cl>
2021-08-14 21:36:08 -0400

    Primeros pasos en Spring Boot IDE IntelliJ IDEA

---
+++ src/main/java/cl/plaxer/sringboot/webapp/SpringbootGuruSection01Application.java
@@ -0,0 +1,13 @@
+package cl.plaxer.sringboot.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGuruSection01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGuruSection01Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/plaxer/sringboot/webapp/SpringbootGuruSection01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.plaxer.sringboot.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGuruSection01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
